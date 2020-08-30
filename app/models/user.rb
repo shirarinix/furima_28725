d6c54863eec147d12ca4f1f7a44e93d9cdf11ce8
shirarinix <yukatoyoshirin@gmail.com>
2020-08-31 02:57:27 +0900

    ユーザー機能実装②(エラー表示、正規表現)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, :email, presence: true
+
   has_many :items
   has_many :purchases
+
+  validates :nickname, :email, :birthday, presence: true
+  with_options presence: true do
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+    validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :sex_kana, :name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+  end
 end
