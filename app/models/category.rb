fb04ea0da5d70030d0a5a140e5299d3612504bb5
shirarinix <yukatoyoshirin@gmail.com>
2020-09-02 23:32:55 +0900

    商品出品機能(アクティブハッシュ)category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
-class Genre < ActiveHash::Base
+class Category < ActiveHash::Base
 self.data = [
   { id: 0, name: '--' },
   { id: 1, name: 'レディース' },
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,13 +2,14 @@ class Item < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
   has_one :purchas
 
-  validates :image, :name, :introduction, :selling_price, presence: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
+  belongs_to_active_hash :category
 
   #空の投稿を保存できないようにする
-  validates :prefecture, presence: true
+  validates :image, :name, :introduction, :selling_price, presence: true
+  validates :prefecture, :category, presence: true
 
   #ジャンルの選択が「--」の時は保存できないようにする
-  validates :prefecture_id, numericality: { other_than: 0 }
+  validates :category_id, :prefecture_id, numericality: { other_than: 0 }
 end
