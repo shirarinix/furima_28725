75e3e6a9edf9d6ef7dd0151f32e16ba97020e9e3
shirarinix <yukatoyoshirin@gmail.com>
2020-09-02 22:30:33 +0900

    商品出品機能(アクティブハッシュ)Prefecture

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,19 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
   end 
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -18,5 +26,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image,:name,:introduction,:prefecture_id,:user_id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,14 @@
 class Item < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, foreign_key: 'user_id'
   has_one :purchas
 
   validates :image, :name, :introduction, :selling_price, presence: true
-  # extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to_active_hash :item
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 
-  # #空の投稿を保存できないようにする
-  # validates :item, presence: true
+  #空の投稿を保存できないようにする
+  validates :prefecture, presence: true
 
-  # #ジャンルの選択が「--」の時は保存できないようにする
-  # validates :prefecture_id, numericality: { other_than: 0 }
+  #ジャンルの選択が「--」の時は保存できないようにする
+  validates :prefecture_id, numericality: { other_than: 0 }
 end
