2a9df14b48a9f58c30b7ad300bca87d9be0219dd
shirarinix <yukatoyoshirin@gmail.com>
2020-09-02 15:15:27 +0900

    商品出品機能(routing、controller、model)の仮設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one :purchas
+
+  validates :image, :name, :introduction, :selling_price, presence: true
+  # extend ActiveHash::Associations::ActiveRecordExtensions
+  # belongs_to_active_hash :item
+
+  # #空の投稿を保存できないようにする
+  # validates :item, presence: true
+
+  # #ジャンルの選択が「--」の時は保存できないようにする
+  # validates :prefecture_id, numericality: { other_than: 0 }
 end
