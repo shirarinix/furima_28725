2aad6565c9208e4a130608f31c93c5864da20ece
shirarinix <yukatoyoshirin@gmail.com>
2020-09-03 00:57:28 +0900

    商品出品機能(アクティブハッシュ)delivery

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,11 +6,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :category
   belongs_to_active_hash :product
+  belongs_to_active_hash :delivery
 
   # 空の投稿を保存できないようにする
-  validates :image, :name, :introduction, :selling_price, presence: true
+  validates :delivery, :image, :name, :introduction, :selling_price, presence: true
   validates :product, :category, :prefecture, presence: true
 
   # ジャンルの選択が「--」の時は保存できないようにする
-  validates :product_id, :category_id, :prefecture_id, numericality: { other_than: 0 }
+  validates :delivery_id, :product_id, :category_id, :prefecture_id, numericality: { other_than: 0 }
 end
