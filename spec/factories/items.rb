478822e579c64e4892fd51e62c481d8318e1904c
shirarinix <yukatoyoshirin@gmail.com>
2020-09-04 02:49:57 +0900

    商品出品機能(FactoryBot作成)

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
-  factory :items do
-    
+  factory :item do
+    image            { Faker::Avatar.image }
+    name             { Faker::Internet.domain_word }
+    introduction     { Faker::Lorem.paragraph(sentence_count = １) }
+    category_id      { Faker::Number.within(range: 1..10) }
+    product_id       { Faker::Number.within(range: 1..10) }
+    delivery_id      { Faker::Number.within(range: 1..10) }
+    prefecture_id    { Faker::Number.within(range: 1..10) }
+    preparation_id   { Faker::Number.within(range: 1..10) }
+    selling_price    { Faker::Number.within(range: 300..9999999 }
   end
 end
