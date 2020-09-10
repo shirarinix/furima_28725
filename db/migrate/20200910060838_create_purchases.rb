d5a8b6f751862d595e41fd37e3976445cf91e3ad
shirarinix <yukatoyoshirin@gmail.com>
2020-09-10 16:03:32 +0900

    商品購入機能モデル作成

---
+++ db/migrate/20200910060838_create_purchases.rb
@@ -0,0 +1,9 @@
+class CreatePurchases < ActiveRecord::Migration[6.0]
+  def change
+    create_table :purchases do |t|
+      t.integer :user_id,  null: false
+      t.integer :item_id,  null: false
+      t.timestamps
+    end
+  end
+end
